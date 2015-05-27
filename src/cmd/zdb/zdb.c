4f9c87c8c735ff80e40708ae505398832d57a90e
Gordan Bobic <gordan@redsleeve.org>
2015-05-27 21:11:10 +0100

    zdb printf formatting patch from Fedora (zfs-fuse-printf-format.patch)

--- src/cmd/zdb/zdb.c
+++ src/cmd/zdb/zdb.c
@@ -1659,7 +1659,7 @@ dump_uberblock(uberblock_t *ub, const char *header, const char *footer)
 {
 	time_t timestamp = ub->ub_timestamp;
 
-	(void) printf(header ? header : "");
+	(void) printf(header ? "%s", header : "%s", "");
 	(void) printf("\tmagic = %016llx\n", (u_longlong_t)ub->ub_magic);
 	(void) printf("\tversion = %llu\n", (u_longlong_t)ub->ub_version);
 	(void) printf("\ttxg = %llu\n", (u_longlong_t)ub->ub_txg);
@@ -1671,7 +1671,7 @@ dump_uberblock(uberblock_t *ub, const char *header, const char *footer)
 		sprintf_blkptr(blkbuf, &ub->ub_rootbp);
 		(void) printf("\trootbp = %s\n", blkbuf);
 	}
-	(void) printf(footer ? footer : "");
+	(void) printf(footer ? "%s", footer : "%s", "");
 }
 
 static void
